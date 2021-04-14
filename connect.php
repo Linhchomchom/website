e4ee35bebf09245f36b68ad148586040cdadf5d6
Linhchomchom <82081662+Linhchomchom@users.noreply.github.com>
2021-04-14 12:40:14 +0700

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,3 @@
+<?php
+    $db_connection = pg_connect("host=ec2-52-50-171-4.eu-west-1.compute.amazonaws.com port=5432 dbname=dd8hosmhhucttl user=hhkznuwwepttwp password=88d45ebb4190b2a1a32acf7747c9e662312fdbd930def2fca357291bba06cdf8");
+?>
